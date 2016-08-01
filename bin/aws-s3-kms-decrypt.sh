c76e734c027d2bfbfe939b747637e321db09c2d2
Eugene Myasyshchev <Eugene.Myasyshchev@dataart.com>
2016-08-01 15:04:40 +0300

    Adding script to get from aws s3 and decrypt.

---
+++ bin/aws-s3-kms-decrypt.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# fail fast
+set -e
+
+CMD=`basename $0`
+TMPFILE=`mktemp /tmp/${CMD}.XXXXXX` || exit 1
+
+echo "Copying from S3 $1 to ${TMPFILE}"
+aws s3 cp --quiet $1 ${TMPFILE}
+
+echo "Decrypting ${TMPFILE} to $2"
+base64 --decode ${TMPFILE} > ${TMPFILE}.plain
+aws kms decrypt --ciphertext-blob fileb://${TMPFILE}.plain --query Plaintext --output text | base64 --decode > $2
